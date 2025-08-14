52ad9302941f8fc32784844afe29e6e850c6895b
bashlui <luisboltech005@outlook.com>
2025-08-14 15:16:07 -0600

    funciona correctamente en port 8000

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"message": "hola me llamo toño está jalando a toda madre"})
+    return jsonify({"message": "hola me llamo tono y funciona!"})
 
 if __name__ == '__main__':
     app.run(threaded=True, host='0.0.0.0', port=3000)
